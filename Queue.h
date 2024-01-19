bdfbddfbf021273a75171f4ee0c61b5870d46aa4
Dave West <dawest6674@eagle.fgcu.edu>
2024-01-18 19:48:20 -0500

    Queue class divided into header for declarations and .cpp for definitions.

--- Queue.h
+++ Queue.h
@@ -8,10 +8,10 @@
 #ifndef QUEUE_DATATYPE
 #define QUEUE_DATATYPE
 
-#include "List.h"
+#include "Doubly_Linked_List.h"
 
 template <typename T>
-class Queue : protected List<T> {
+class Queue : protected Doubly_Linked_List<T> {
 
 public:
     // Constructors
@@ -24,69 +24,5 @@ public:
     T peek();
     int size();
 };
-
-
-// -----------------------------------------------------------------------------
-// Function Definitions
-// -----------------------------------------------------------------------------
-
-/**
- * Default constructor
- */
-template <typename T>
-Queue<T>::Queue() {
-    List<T>::headNode = nullptr;
-    List<T>::tailNode = nullptr;
-    List<T>::_size = 0;
-}
-
-
-/**
- * Destructor
- */
-template <typename T>
-Queue<T>::~Queue( ) {
-    List<T>::makeEmpty( );
-}
-
-
-/**
- * Adds a node with the given value to the back of the queue.
- * @param data (int) - The value to be held by the node.
- */
-template <typename T>
-void Queue<T>::enqueue( T data ) {
-    List<T>::insert( data, List<T>::_size );
-}
-
-
-/**
- * Removes the first node from the queue.
- * @return (int) The value of the deleted node.
- */
-template <typename T>
-T Queue<T>::dequeue( ) {
-    return List<T>::remove( 0 );
-}
-
-
-/**
- * Returns the value of the first node of the queue.
- * @return (int) - The value of the first node of the queue.
- */
-template <typename T>
-T Queue<T>::peek( ) {
-    return List<T>::headNode -> value;
-}
-
-
-/**
- * Returns the number of nodes in the queue.
- * @return (int) - The number of nodes in the queue.
- */
-template <typename T>
-inline int Queue<T>::size( ) {
-    return List<T>::_size;
-}
 
 #endif //QUEUE_DATATYPE
